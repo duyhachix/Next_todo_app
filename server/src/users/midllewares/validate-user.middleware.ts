940ccc34728bafc8f0c67305446f9dbba8ba58bd
Thanhduy_Hachi-x <duy.nguyen@hachi-x.com>
2024-01-12 10:03:56 +0700

    :icecream: create user middleware (dummy authorization)

--- server/src/users/midllewares/validate-user.middleware.ts
+++ server/src/users/midllewares/validate-user.middleware.ts
@@ -1,6 +1,25 @@
-// import { NestMiddleware } from '@nestjs/common';
-// import { UsersService } from '../users.service';
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
 
-// export class ValidateUserMiddleware implements NestMiddleware {
-//   constructor(private readonly userService: UsersService) {}
-// }
+@Injectable()
+export class ValidateUserMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    // perform validation logic
+    // check if user has jwt token, authorization
+
+    const { authorization } = req.headers;
+    if (!authorization) {
+      return res
+        .status(403)
+        .send({ error: 'There no authorization token provided' });
+    }
+    // sau nay khi co authorization
+    if (authorization === `123`) {
+      next();
+    } else {
+      return res
+        .status(403)
+        .send({ error: 'Invalid authorization token provided' });
+    }
+  }
+}
--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,15 +1,33 @@
 // standard libraries
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 // internal libraries
 import { UsersService } from './users.service';
 import { Users } from '../database/entity/users.entity';
 import { UsersController } from './users.controller';
+import { ValidateUserMiddleware } from '../users/midllewares/validate-user.middleware';
+// import { ValidateUserAccount } from './midllewares/validate-user-account.middleware'; // for the test purpose
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, UsersService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // apply middleware for routes that listed below
+    consumer
+      .apply(
+        ValidateUserMiddleware,
+        // ValidateUserAccount,
+      )
+      .exclude({ path: 'users', method: RequestMethod.GET }) // exclude "/user" route out of the middleware
+      .forRoutes(UsersController);
+  }
+}
