dacff486ea643b945ddc95dbe74aea3ef7d16f9d
Thanhduy_Hachi-x <duy.nguyen@hachi-x.com>
2024-01-16 16:54:13 +0700

    :sparkles: init commit client

--- client/app/page.js
+++ client/app/page.js
@@ -1,113 +1,267 @@
-import Image from 'next/image'
+'use client';
+import { useState } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	// state
+	const [user, setUser] = useState(null);
+	const [isLoginForm, setIsLoginForm] = useState(true);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [newEmail, setNewEmail] = useState('');
+	const [newUsername, setNewUsername] = useState('');
+	const [newPassword, setNewPassword] = useState('');
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	async function handleLogin(e) {
+		e.preventDefault();
+		let loginInfo = {
+			email,
+			password,
+		};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		try {
+			let response = await axios.post(
+				`http://localhost:8080/auth/signin`,
+				loginInfo
+			);
+			if (response.status === 200) {
+				let user = response.data.authUser;
+				setUser(user);
+				toast.success(response.data?.message, {
+					position: 'top-right',
+					autoClose: 3000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: 'light',
+				});
+				setEmail('');
+				setPassword('');
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-				<a
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Learn{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Learn about Next.js in an interactive course with&nbsp;quizzes!
-					</p>
-				</a>
+	async function handleSignup(e) {
+		e.preventDefault();
+		let signupInfo = {
+			email: newEmail,
+			username: newUsername,
+			password: newPassword,
+		};
 
-				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Templates{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Explore starter templates for Next.js.
-					</p>
-				</a>
+		try {
+			let response = await axios.post(
+				`http://localhost:8080/users/signup`,
+				signupInfo
+			);
+			if (response.status === 200) {
+				toast.success(response.data?.message, {
+					position: 'top-right',
+					autoClose: 3000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: 'light',
+				});
+			}
+		} catch (error) {
+			console.log(error);
+		}
+		console.log('signup info', signupInfo);
+	}
 
-				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Deploy{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Instantly deploy your Next.js site to a shareable URL with Vercel.
-					</p>
-				</a>
-			</div>
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			{isLoginForm ? (
+				// login form
+				<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+					<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+						<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+							Sign in to your account
+						</h2>
+					</div>
+
+					<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+						<form className="space-y-6" action="#" onSubmit={handleLogin}>
+							<div>
+								<label
+									htmlFor="email"
+									className="block text-sm font-medium leading-6 text-gray-900"
+								>
+									Email address
+								</label>
+								<div className="mt-2">
+									<input
+										required
+										type="email"
+										value={email}
+										onChange={(e) => {
+											setEmail(e.target.value);
+										}}
+										className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									/>
+								</div>
+							</div>
+
+							<div>
+								<div className="flex items-center justify-between">
+									<label
+										htmlFor="password"
+										className="block text-sm font-medium leading-6 text-gray-900"
+									>
+										Password
+									</label>
+									<div className="text-sm">
+										<a
+											href="#"
+											className="font-semibold text-indigo-600 hover:text-indigo-500"
+										>
+											Forgot password?
+										</a>
+									</div>
+								</div>
+								<div className="mt-2">
+									<input
+										required
+										type="password"
+										value={password}
+										onChange={(e) => {
+											setPassword(e.target.value);
+										}}
+										className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									/>
+								</div>
+							</div>
+
+							<div>
+								<button
+									type="submit"
+									className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+								>
+									Sign in
+								</button>
+							</div>
+						</form>
+
+						<p className="mt-10 text-center text-sm text-gray-500">
+							Not a member?{' '}
+							<a
+								href="#"
+								className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+								onClick={() => {
+									setIsLoginForm(false);
+								}}
+							>
+								Sign up
+							</a>
+						</p>
+					</div>
+				</div>
+			) : (
+				// sign up form
+				<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+					<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+						<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+							Sign up a your new accout
+						</h2>
+					</div>
+
+					<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+						<form className="space-y-6" action="#" onSubmit={handleSignup}>
+							<div>
+								<label
+									htmlFor="newUsername"
+									className="block text-sm font-medium leading-6 text-gray-900"
+								>
+									Username
+								</label>
+								<div className="mt-2">
+									<input
+										required
+										type="text"
+										value={newUsername}
+										onChange={(e) => {
+											setNewUsername(e.target.value);
+										}}
+										className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									/>
+								</div>
+							</div>
+							<div>
+								<label
+									htmlFor="newEmail"
+									className="block text-sm font-medium leading-6 text-gray-900"
+								>
+									Email address
+								</label>
+								<div className="mt-2">
+									<input
+										required
+										type="email"
+										value={newEmail}
+										onChange={(e) => {
+											setNewEmail(e.target.value);
+										}}
+										className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									/>
+								</div>
+							</div>
+							<div>
+								<div className="flex items-center justify-between">
+									<label
+										htmlFor="newPassword"
+										className="block text-sm font-medium leading-6 text-gray-900"
+									>
+										Password
+									</label>
+								</div>
+								<div className="mt-2">
+									<input
+										required
+										type="password"
+										value={newPassword}
+										onChange={(e) => {
+											setNewPassword(e.target.value);
+										}}
+										className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									/>
+								</div>
+							</div>
+
+							<div>
+								<button
+									type="submit"
+									className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+								>
+									Sign up
+								</button>
+							</div>
+						</form>
+
+						<p className="mt-10 text-center text-sm text-gray-500">
+							Already have an account ?{' '}
+							<a
+								href="#"
+								className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+								onClick={() => {
+									setIsLoginForm(true);
+								}}
+							>
+								Sign in
+							</a>
+						</p>
+					</div>
+				</div>
+			)}
+			<ToastContainer />
 		</main>
 	);
 }
